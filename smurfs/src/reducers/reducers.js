8e3ec0248aae85a509947ec3f60f34d830f0acae
Impulse2020 <irishht@gmail.com>
2020-06-12 11:40:52 -0400

    Builds out reducer and installs redux

---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,25 @@
+
+const initialStore = {
+    name:"",
+    Job:"",
+    Nickname:"",
+    isFETCHING:"",
+    eroor:''
+}
+
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case "":
+        
+
+
+        default:
+            return state;
+    }
+
+
+}
+
+
+export default reducer;
